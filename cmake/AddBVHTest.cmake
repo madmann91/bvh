d1830fc4a3b31fadfdf8f3f7edb180daf8d664d2
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2020-04-10 14:20:31 +0200

    Refactor CMake logic. - Follow CMake coding style - Skip compiling tests when importing this project into another one - Skip compiling tests when BUILD_TESTING is OFF - Add compiler options to function add_bvh_test

--- cmake/AddBVHTest.cmake
+++ cmake/AddBVHTest.cmake
@@ -1,50 +1,45 @@
-# Adds an executable target, links 'bvh',
-# adds compiler flags, and makes it a test.
-# It takes several types of arguments.
+# Adds an executable target, links 'bvh' to it, adds compiler flags,
+# and makes it a test. Arguments are:
 #
 #   SOURCES : The source files used to compile the program.
-#   ARGS    : The arguments to pass to the test invocation.
+#   OPTIONS : Optional. Compiler options to use when compiling the program.
+#   ARGS    : Optional. The arguments to pass to the test invocation.
 #   NAME    : The name of the test program. The prefix 'bvh_'
 #             is prepended to this when declaring the CMake target.
-function(add_bvh_test)
+function (add_bvh_test)
+    set(single_value_args NAME)
+    set(multi_value_args SOURCES ARGS OPTIONS)
 
-  set(single_value_args NAME)
+    cmake_parse_arguments(bvh_test "" "${single_value_args}" "${multi_value_args}" "${ARGN}")
 
-  set(multi_value_args SOURCES ARGS)
+    if ("${bvh_test_NAME}" STREQUAL "")
+        message(FATAL_ERROR "Missing 'NAME' parameter. Please specify test name.")
+    endif ()
 
-  cmake_parse_arguments(test "" "${single_value_args}" "${multi_value_args}" "${ARGN}")
+    if ("${bvh_test_SOURCES}" STREQUAL "")
+        message(FATAL_ERROR "Missing 'SOURCES' parameter. Please specify what sources to compile.")
+    endif ()
 
-  if("${test_NAME}" STREQUAL "")
-    message(FATAL_ERROR "Missing 'NAME' parameter. Please specify test name.")
-  endif("${test_NAME}" STREQUAL "")
+    # CMake doesn't scope target names.
+    # When this project is added as a sub directory
+    # to another project, adding the 'bvh_' prefix
+    # prevents target names from colliding.
+    set(target_name bvh_${bvh_test_NAME})
 
-  if("${test_SOURCES}" STREQUAL "")
-    message(FATAL_ERROR "Missing 'SOURCES' parameter. Please specify what sources to compile.")
-  endif("${test_SOURCES}" STREQUAL "")
+    add_executable(${target_name} ${bvh_test_SOURCES})
+    target_link_libraries(${target_name} PRIVATE bvh)
 
-  # CMake doesn't scope target names.
-  # When this project is added as a sub directory
-  # to another project, adding the 'bvh_' prefix
-  # prevents target names from colliding.
-  set(target_name bvh_${test_NAME})
+    # Here we tell CMake that we want the filename to
+    # be called ${bvh_test_NAME} (with .exe on Windows) and
+    # that we want it to appear in the project binary directory
+    # so that the user can easily run it.
+    set_target_properties(${target_name} PROPERTIES
+        OUTPUT_NAME ${bvh_test_NAME}
+        RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR})
 
-  add_executable(${target_name} ${test_SOURCES})
+    target_compile_options(${target_name} PRIVATE ${bvh_test_OPTIONS})
 
-  target_link_libraries(${target_name} PRIVATE bvh)
-
-  # Here we tell CMake that we want the filename to
-  # be called ${test_name} (with .exe on Windows) and
-  # that we want it to appear in the project binary directory
-  # so that the user can easily run it.
-  set_target_properties(${target_name} PROPERTIES
-    OUTPUT_NAME ${test_NAME}
-    RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR})
-
-  # 'bvh_cxxflags' is set in the parent CMakeLists.txt
-  target_compile_options(${target_name} PRIVATE ${bvh_cxxflags})
-
-  add_test(NAME ${target_name}
-           COMMAND $<TARGET_FILE:${target_name}> ${test_ARGS}
-           WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
-
-endfunction(add_bvh_test test_name)
+    add_test(NAME ${target_name}
+        COMMAND $<TARGET_FILE:${target_name}> ${bvh_test_ARGS}
+        WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+endfunction ()
