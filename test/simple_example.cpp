df95019e4d785c53f9806b8a66452f70e69aa258
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2021-08-19 13:06:25 +0200

    Improve simple example

--- test/simple_example.cpp
+++ test/simple_example.cpp
@@ -1,9 +1,11 @@
 #include <vector>
 #include <iostream>
+#include <type_traits>
 
 #include <bvh/bvh.hpp>
 #include <bvh/vector.hpp>
 #include <bvh/triangle.hpp>
+#include <bvh/sphere.hpp>
 #include <bvh/ray.hpp>
 #include <bvh/sweep_sah_builder.hpp>
 #include <bvh/single_ray_traverser.hpp>
@@ -12,11 +14,53 @@
 using Scalar   = float;
 using Vector3  = bvh::Vector3<Scalar>;
 using Triangle = bvh::Triangle<Scalar>;
+using Sphere   = bvh::Sphere<Scalar>;
 using Ray      = bvh::Ray<Scalar>;
 using Bvh      = bvh::Bvh<Scalar>;
 
+// This function builds a BVH from a vector of primitives, and intersects it with a ray.
+template <typename Primitive>
+static bool build_and_intersect_bvh(const std::vector<Primitive>& primitives) {
+    Bvh bvh;
+
+    // Compute the global bounding box and the centers of the primitives.
+    // This is the input of the BVH construction algorithm.
+    // Note: Using the bounding box centers instead of the primitive centers is possible,
+    // but usually leads to lower-quality BVHs.
+    auto [bboxes, centers] = bvh::compute_bounding_boxes_and_centers(primitives.data(), primitives.size());
+    auto global_bbox = bvh::compute_bounding_boxes_union(bboxes.get(), primitives.size());
+
+    // Create an acceleration data structure on the primitives
+    bvh::SweepSahBuilder<Bvh> builder(bvh);
+    builder.build(global_bbox, bboxes.get(), centers.get(), primitives.size());
+
+    // Intersect a ray with the data structure
+    Ray ray(
+        Vector3(0.0, 0.0, 0.0), // origin
+        Vector3(0.0, 0.0, 1.0), // direction
+        0.0,                    // minimum distance
+        100.0                   // maximum distance
+    );
+    bvh::ClosestPrimitiveIntersector<Bvh, Primitive> primitive_intersector(bvh, primitives.data());
+    bvh::SingleRayTraverser<Bvh> traverser(bvh);
+
+    if (auto hit = traverser.traverse(ray, primitive_intersector)) {
+        auto triangle_index = hit->primitive_index;
+        auto intersection = hit->intersection;
+        std::cout
+            << "Hit primitive " << triangle_index << "\n"
+            << "distance: "    << intersection.t << "\n";
+        if constexpr (std::is_same_v<Primitive, Triangle>) {
+            std::cout
+                << "u: "           << intersection.u << "\n"
+                << "v: "           << intersection.v << "\n";
+        }
+        return true;
+    }
+    return false;
+}
+
 int main() {
-    // Create an array of triangles
     std::vector<Triangle> triangles;
     triangles.emplace_back(
         Vector3( 1.0, -1.0, 1.0),
@@ -28,34 +72,13 @@ int main() {
         Vector3(-1.0, -1.0, 1.0),
         Vector3(-1.0,  1.0, 1.0)
     );
+    if (!build_and_intersect_bvh(triangles))
+        return 1;
 
-    Bvh bvh;
-
-    // Create an acceleration data structure on those triangles
-    bvh::SweepSahBuilder<Bvh> builder(bvh);
-    auto [bboxes, centers] = bvh::compute_bounding_boxes_and_centers(triangles.data(), triangles.size());
-    auto global_bbox = bvh::compute_bounding_boxes_union(bboxes.get(), triangles.size());
-    builder.build(global_bbox, bboxes.get(), centers.get(), triangles.size());
-
-    // Intersect a ray with the data structure
-    Ray ray(
-        Vector3(0.0, 0.0, 0.0), // origin
-        Vector3(0.0, 0.0, 1.0), // direction
-        0.0,                    // minimum distance
-        100.0                   // maximum distance
-    );
-    bvh::ClosestPrimitiveIntersector<Bvh, Triangle> primitive_intersector(bvh, triangles.data());
-    bvh::SingleRayTraverser<Bvh> traverser(bvh);
-
-    auto hit = traverser.traverse(ray, primitive_intersector);
-    if (hit) {
-        auto triangle_index = hit->primitive_index;
-        auto intersection = hit->intersection;
-        std::cout << "Hit triangle " << triangle_index << "\n"
-                  << "distance: "    << intersection.t << "\n"
-                  << "u: "           << intersection.u << "\n"
-                  << "v: "           << intersection.v << std::endl;
-        return 0;
-    }
-    return 1;
+    std::vector<Sphere> spheres;
+    spheres.emplace_back(Vector3( 1.0, -1.0, 1.0), 1.0);
+    spheres.emplace_back(Vector3( 0.0,  0.0, 2.0), 0.5);
+    if (!build_and_intersect_bvh(spheres))
+        return 1;
+    return 0;
 }
